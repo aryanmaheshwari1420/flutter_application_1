bb81a64c104ba477d1d6b3c660194538b0969fe7
Aryan Maheshwari <98485902+aryanmaheshwari1420@users.noreply.github.com>
2022-06-26 14:34:10 +0530

    Delete lib/pizzaApp directory

--- lib/pizzaApp/pizzaMain.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_application_1/pizzaApp/pizzaApp.dart';
-
-void main() {
-  runApp(MaterialApp(
-    home: pizzaApp(),
-  ));
-}
